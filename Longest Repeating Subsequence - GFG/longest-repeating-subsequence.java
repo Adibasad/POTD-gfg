557cb3517d99f70b0b72e2bb44099aa53e533790
Adiba sadaf <125906596+Adibasad@users.noreply.github.com>
2023-07-19 15:54:40 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
@@ -33,12 +33,12 @@ class Solution
          String s = str;
      int n = str.length();
      int [][]dp= new int[n+1][n+1];
-     for(int i = 0;i<=n;i++){
-         dp[i][0] = 0;
-     }
-     for(int i = 0;i<=n;i++){
-         dp[0][i] = 0;
-     }
+    //  for(int i = 0;i<=n;i++){
+    //      dp[i][0] = 0;
+    //  }
+    //  for(int i = 0;i<=n;i++){
+    //      dp[0][i] = 0;
+    //  }
      for(int i = 1;i<=n;i++){
          for(int j =1;j<=n;j++){
              if(str.charAt(i-1) == str.charAt(j-1) && i != j){
