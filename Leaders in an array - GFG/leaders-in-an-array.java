f79f358e266945a1f54a73e19fde29887bf847dd
Adiba sadaf <125906596+Adibasad@users.noreply.github.com>
2023-08-18 18:22:56 +0530

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+
+		    for(int i=0; i<res.size(); i++){
+		        ot.print(res.get(i)+" ");
+		    }
+		    
+		    ot.println();
+		}
+		ot.close();
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(n==0) return ans;
+        
+        int lastLead=arr[n-1];
+        ans.add(arr[n-1]);
+        
+        if(n==1) return ans;
+        
+        int i=n-2;
+        while(i>=0)
+        {
+            if(arr[i]>=lastLead){
+                ans.add(arr[i]);
+                lastLead=arr[i];
+            }
+            
+            i--;
+        }
+        
+        // System.out.println(ans);
+        Collections.reverse(ans);
+        return ans;
+    }
+}
