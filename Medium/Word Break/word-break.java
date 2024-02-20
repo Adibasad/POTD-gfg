ba060b23ab10673d92c978dbca03aeb6b33fd567
Adiba sadaf <125906596+Adibasad@users.noreply.github.com>
2024-02-20 08:47:04 -0800

    Added Solution - GfG to GitHub

---
+++ Medium/Word Break/word-break.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+public class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n;
+                    n = sc.nextInt();
+                    ArrayList<String> dictionary = new ArrayList<String>();
+                    for(int i = 0;i<n;i++)
+                        {
+                            String p = sc.next();
+                            dictionary.add(p);
+                        }
+                    String s = sc.next();
+                    Solution obj = new Solution();  
+                    System.out.println(obj.wordBreak(n, s, dictionary));  
+                    
+                }
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static int wordBreak(int n, String s, ArrayList<String> dictionary )
+    {
+        //code here
+        int len=s.length();
+        boolean dp[]=new boolean[len+1];
+        dp[0]=true;
+        for(int i=0;i<=len;i++){
+            for(int j=0;j<=i;j++){
+                if(dp[j] && dictionary.contains(s.substring(j,i))) dp[i]=true;
+            }
+        }
+        return dp[len]?1:0;
+    }
+}
