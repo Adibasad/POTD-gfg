3737d1f60e6dba0770e6a5f3a879e81b937105c3
Adiba sadaf <125906596+Adibasad@users.noreply.github.com>
2024-03-02 08:45:04 -0800

    Added Solution - GfG to GitHub

---
+++ Easy/First element to occur k times/first-element-to-occur-k-times.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class GfG { 
+	public static void main (String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int k = Integer.parseInt(inputLine[1]);
+		    int[] arr = new int[n];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++)arr[i] = Integer.parseInt(inputLine[i]);
+		    
+		    Solution obj = new Solution();
+		    System.out.println(obj.firstElementKTime(n, k, arr));
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int firstElementKTime(int n, int k, int[] a) { 
+        
+        if(k==1) return a[0];
+        
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            int ele=a[i];
+            if(!map.containsKey(ele)) map.put(ele,1);
+            
+            else 
+            {
+                map.put(ele , map.get(ele) + 1);
+                if(map.get(ele)==k) return ele;
+            }
+        }
+        
+        
+        return -1;
+        
+    } 
+}
