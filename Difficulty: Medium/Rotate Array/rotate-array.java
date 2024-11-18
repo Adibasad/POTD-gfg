2b81066bf64aa001c82eb27f360d64ca3b7c4ece
Adiba sadaf <125906596+Adibasad@users.noreply.github.com>
2024-11-18 22:48:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        int n=arr.length;
+        
+        Queue<Integer> que = new LinkedList<>();
+        for(int x: arr) que.add(x);
+        
+        for(int i=0;i<d%n;i++)
+        {
+            int temp=que.poll();
+            que.add(temp);
+            // System.out.println(que);
+        }
+        
+        for(int i=0;i<n;i++) arr[i]=que.poll();
+    }
+}
