fa95b1169d8f5e4e6c3f6ec244452753f7a6ba19
Adiba sadaf <125906596+Adibasad@users.noreply.github.com>
2025-04-26 12:31:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Is Binary Tree Heap/is-binary-tree-heap.java
@@ -0,0 +1,149 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null) return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Solution ob = new Solution();
+            if (ob.isHeap(root))
+                System.out.println("true");
+            else
+                System.out.println("false");
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User Function template for JAVA
+
+/*
+Node defined as
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+
+class Solution {
+    boolean isHeap(Node tree) {
+        // code here
+        Queue<Node> q=new LinkedList<>();
+        q.add(tree);
+        while(!q.isEmpty()){
+            int n=q.size();
+            boolean nullChild=false;
+            for(int i=0;i<n;i++){
+                Node node=q.poll();
+                
+                if(node.left!=null){
+                    if(nullChild) return false;
+                    if(node.data<node.left.data) return false;
+                    q.add(node.left);
+                }
+                else nullChild=true;
+                
+                if(node.right!=null){
+                    if(nullChild) return false;
+                    if(node.data<node.right.data) return false;
+                    q.add(node.right);
+                }
+                else nullChild=true;
+            }
+        }
+        return true;
+    }
+}
